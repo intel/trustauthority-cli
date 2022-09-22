87afddb465eecd09aaa11ca1530ca5a0005cd497
arijit8972 <arijit.ghosh@intel.com>
2022-09-22 15:47:44 +0530

    Mark subscription ID as required in Update Subscription command (#16)

--- cmd/update_subscription.go
+++ cmd/update_subscription.go
@@ -56,6 +56,7 @@ func init() {
 	updateSubscriptionCmd.MarkFlagRequired(constants.ServiceIdParamName)
 	updateSubscriptionCmd.MarkFlagRequired(constants.ProductIdParamName)
 	updateSubscriptionCmd.MarkFlagRequired(constants.SubscriptionDescriptionParamName)
+	updateSubscriptionCmd.MarkFlagRequired(constants.SubscriptionIdParamName)
 }
 
 func updateSubscription(cmd *cobra.Command) (string, error) {
